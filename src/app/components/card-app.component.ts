ad892b54858a90e93e06f8c7110bbb5b9ad10c4c
Bl4dimir98 <bladimiralex@hotmail.com>
2025-04-22 11:33:45 -0500

    implement cart component

--- src/app/components/card-app.component.ts
+++ src/app/components/card-app.component.ts
@@ -2,16 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { Product } from '../models/product';
 import { CatalogueComponent } from "./catalogue/catalogue.component";
+import { CartComponent } from './cart/cart.component';
+import { CartItem } from '../models/cart-item';
 
 @Component({
   selector: 'card-app',
   standalone: true,
-  imports: [CatalogueComponent],
+  imports: [CatalogueComponent, CartComponent],
   templateUrl: './card-app.component.html'
 })
 export class CardAppComponent implements OnInit {
 
   products: Product[] = [];
+
+  items: CartItem[] = [];
 
   constructor(private productService: ProductService) { }
 
