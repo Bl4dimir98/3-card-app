9a02185ea1cdc8ad98a111a1b5fd647177830e7a
Bl4dimir98 <bladimiralex@hotmail.com>
2025-04-24 10:37:47 -0500

    add saveSession

--- src/app/components/cart-app.component.ts
+++ src/app/components/cart-app.component.ts
@@ -23,6 +23,7 @@ export class CartAppComponent implements OnInit {
 
   ngOnInit(): void {
     this.products = this.productService.findAll();
+    this.items = JSON.parse(sessionStorage.getItem('cart')!) || [];
     this.calculateTotal();
   }
 
@@ -42,15 +43,21 @@ export class CartAppComponent implements OnInit {
       this.items = [... this.items, { product: { ...product }, quantity: 1 }];
     }
     this.calculateTotal();
+    this.saveSession();
   }
 
   onDeleteCart(id: number): void {
     this.items = this.items.filter(item => item.product.id !== id);
     this.calculateTotal();
+    this.saveSession();
   }
 
   calculateTotal(): void {
     this.total = this.items.reduce((acum, item) => acum + item.quantity * item.product.price, 0);
+  }
+
+  saveSession(): void {
+    sessionStorage.setItem('cart', JSON.stringify(this.items));
   }
 
 }
