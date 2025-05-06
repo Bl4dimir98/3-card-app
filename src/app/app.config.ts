6b6ae400545cddf0761827fb4d1a125d5352ee51
Bl4dimir98 <bladimiralex@hotmail.com>
2025-05-06 12:20:28 -0500

    implement reducer function

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,15 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideStore } from '@ngrx/store';
+import { itemsReducer } from './store/items.reducer';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+    provideStore({ items: itemsReducer })
+
+  ]
 };
--- src/app/store/items.reducer.ts
+++ src/app/store/items.reducer.ts
@@ -1,5 +1,6 @@
-import { createReducer } from "@ngrx/store"
+import { createReducer, on } from "@ngrx/store"
 import { CartItem } from "../models/cart-item"
+import { add, remove, total } from "./items.actions"
 
 export interface ItemsState {
     items: CartItem[],
@@ -14,4 +15,39 @@ export const initialState: ItemsState = {
 
 export const itemsReducer = createReducer(
     initialState,
+    on(add, (state, { product }) => {
+        const hasItem = state.items.find((item: CartItem) => item.product.id === product.id);
+        if (hasItem) {
+            return {
+                items: state.items.map((item: CartItem) => {
+                    if (item.product.id === product.id) {
+                        return {
+                            ...item,
+                            quantity: item.quantity + 1
+                        }
+                    }
+                    return item;
+                }),
+                total: state.total
+            }
+        } else {
+            return {
+                items: [...state.items, { product: { ...product }, quantity: 1 }],
+                total: state.total
+            };
+        }
+    }),
+    on(remove, (state, { id }) => {
+        return {
+            items: state.items.filter(item => item.product.id !== id),
+            total: state.total
+        }
+    }),
+    on(total, state => {
+        return {
+            items: state.items,
+            total: state.items.reduce((acum, item) => acum + item.quantity * item.product.price, 0)
+        }
+    })
+
 )
